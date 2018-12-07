21595b770a66606144ee946d2dde78e8befad493
isohaven <ridehaven320@gmail.com>
2018-12-06 19:09:48 -0600

    fixed division by zero

--- euler_method.py
+++ euler_method.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 xi = np.float(input('x initial\n:'))
 yi = np.float(input('y initial\n:'))
 
+expression = ''
 expression_valid = False
 while not expression_valid:
     try:
@@ -13,14 +13,19 @@ while not expression_valid:
         expression = input('enter dy/dx in terms of x, y =\n:')
         eval(expression)
         expression_valid = True
+    except ZeroDivisionError:
+        expression_valid = True
     except SyntaxError:
         print('error evaluating dy/dx\n')
 
 
 x_ = np.float(input('end x value\n:'))
 
-dydx = lambda x, y: eval(expression)
-
+def dydx(x, y):
+    try:
+        return eval(expression)
+    except ZeroDivisionError:
+        return np.nan
 
 def euler(xi, yi, x_=0):
 
@@ -43,9 +48,12 @@ def euler(xi, yi, x_=0):
                 break
         dot_size = 25*x_step
         plot(xs, ys, label=('ΔX =', str(x_step)[:5]), size=dot_size)
+        
+        close_index = np.abs(np.asarray(xs) - x_).argmin()
+        plt.scatter(xs[close_index], ys[close_index], c='r', s=50, marker='x')
         x_step /= step_drop_rate
         
-        plt.pause(0.8)
+        plt.pause(0.85)
     plt.show()
 
     # find closest value to x_ in xs and return y[closest] as guess
@@ -55,6 +63,7 @@ def plot(x_list, y_list, label=None, size=10):
     plt.scatter(x_list, y_list, s=size)
     plt.plot(x_list, y_list, label=label)
     plt.legend()
+    plt.title(expression)
 
 guess = euler(xi, yi, x_)
 print('y(', x_, ') is approximately ', sep='', end='')
