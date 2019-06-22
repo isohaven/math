970ebb7e4b6bb7fa0bdd5e04e3aa833f56312d34
isohaven <ridgehaven320@gmail.com>
2019-06-22 00:22:59 -0400

    need to fix nth deriv method

--- taylor.py
+++ taylor.py
@@ -11,11 +11,17 @@ x = np.arange(-10, 10, 0.1)
 # derivitive at point 'c'
 def derive(f, c, h=1e-5):
     return (f(c+h) - f(c-h)) / (2*h)
-
+def nth_derive(f, c, n, h=1e-5):
+    pro = memoized_factorial(n) * (1 / (h**n))
+    summation = 0
+    for m in range(n+1):
+        summation +=  ( (-1)**(m+n) ) * (1/(memoized_factorial(m) * memoized_factorial(n-m) )) * f(c + m*h)
+    return pro*summation
 # plot function
 plt.plot(x, f(x))
 
-def taylor(f, degree):
+# create taylor polynomial centered at 'a'
+def taylor(f, degree, a=0):
     coeffs = []
 
 factorial_cache = {}
@@ -28,3 +34,5 @@ def memoized_factorial(n):
         ans = n * memoized_factorial(n-1)
         factorial_cache[str(n)] = ans
         return ans
+print(derive(f, np.pi/3))
+print(nth_derive(f, np.pi/3, 4, h=1e-6))
