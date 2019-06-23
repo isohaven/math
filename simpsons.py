3f04074d13d6409ad1f8f8a8ad49a6902ea50735
isohaven <ridgehaven320@gmail.com>
2019-06-23 00:06:27 -0500

    added matplotlib visual

--- simpsons.py
+++ simpsons.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 def integrate(f, a=-1, b=1, n=100):
     # implementation from
     # https://www.math.ubc.ca/~pwalls/math-python/integration/simpsons-rule/
@@ -9,5 +11,18 @@ def integrate(f, a=-1, b=1, n=100):
     y = f(x)
     _sum = delta_x/3 * np.sum(y[0:-1:2] + 4*y[1::2] + y[2::2])
     return _sum
+
+
 f = lambda x: np.sin(x)
-print(integrate(f, a=0, b=1))
+
+x = np.linspace(0, 2*np.pi, 150)
+y = f(x)
+_sum = integrate(f, 0, np.pi/2)
+
+interval = np.linspace(0, np.pi/2, 50)
+plt.fill_between(interval,f(interval))
+plt.plot(x, y, label=f'integrel = {round(_sum)}')
+plt.legend()
+
+
+plt.show()
