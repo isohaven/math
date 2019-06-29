9cd097407937aa77bb2a044a2e361c64def538e3
isohaven <ridgehaven320@gmail.com>
2019-06-29 15:53:29 -0500

    cauchy rearranged formula not working

--- taylor.py
+++ taylor.py
@@ -10,16 +10,16 @@ x = np.arange(-10, 10, 0.1)
 # find nth derivitive of function 'f' at point 'c'
 #TODO use Cauchy's integral formula to accurately compute
 # higher order derivitives
-def nth_derivitive(f, c, n, dtheta=1e-3, contour_radius=6):
-    _sum = 0
-    theta = 0
-    while theta < 2*np.pi:
-        f_arg= np.complex128(c + contour_radius*np.exp(1j*theta))
-        factor = (1/contour_radius)**n * np.complex128(np.exp(-1j * n * theta))
-        _sum += f(f_arg) * factor * dtheta
-        theta += dtheta
-    return memoized_factorial(n) * _sum.real / (2*np.pi)
+def nth_derivitive(f, c, n, ):
+  theta = np.linspace(0, 2*np.pi, 951)
+  delta_theta = (2*np.pi - 0) / n
 
+
+  f_arg = np.complex128(c + np.exp(1j*theta))
+  factor = np.complex128(np.exp(-1j * n * theta))
+  integrand = f(f_arg) * factor
+  _sum = delta_theta/3 * np.sum(integrand[0:-1:2] + 4*integrand[1::2] + integrand[2::2])
+  return memoized_factorial(n) * _sum.real / (2*np.pi)
 # plot function
 plt.plot(x, f(x))
 
@@ -37,4 +37,4 @@ def memoized_factorial(n):
         ans = n * memoized_factorial(n-1)
         factorial_cache[str(n)] = ans
         return ans
-print(nth_derivitive(f, np.pi/3, 10))
+print(nth_derivitive(f, np.pi/3, 1))
